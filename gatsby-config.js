ad20b81f1dd98c9da5b3ae3c4bc66d92dd106109
Alexander <alxndr+github@gmail.com>
2023-05-24 20:11:04 -0700

    WIP

---
+++ gatsby-config.js
@@ -0,0 +1,22 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: 'nasin pi lipu nimi',
+    siteUrl: 'https://alxndr.github.io/nasin-pi-lipu-nimi'
+  },
+  plugins: ['gatsby-plugin-sass', {
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      'icon': 'src/images/icon.png'
+    }
+  }, 'gatsby-transformer-remark', {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      'name': 'pages',
+      'path': './src/pages/'
+    },
+    __key: 'pages'
+  }]
+};
---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import * as React from 'react'
+
+const IndexPage = () => {
+  return <>
+    <main>
+      <h1>nasin pi lipu nimi</h1>
+      <p>To identify a glyph, first select a root:</p>
+      <ul>
+      </ul>
+    </main>
+    <footer>
+      <p><a href="https://github.com/alxndr/nasin-pi-lipu-nimi">code</a></p>
+    </footer>
+  </>
+}
+
+export default IndexPage
+
+export const Head = () => <title>Home Page</title>
