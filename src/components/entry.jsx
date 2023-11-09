52708eb498fea10e3e4f4be874ff42be945c57ad
Alexander <alxndr+github@gmail.com>
2023-11-09 13:38:32 -0800

    reword some things

--- src/components/entry.jsx
+++ src/components/entry.jsx
@@ -34,7 +34,7 @@ const EntryComponent = ({glyph, data}) => { // glyph.lasina can be punctuation o
     <div className="entry__sitelenAnte">
       {data && glyph?.lasina &&
         <div className="entry__sitelenPona" lang="tp">
-          <span lang="tp">sitelen ni la o sitelen kepeken sitelen pona e ni</span>
+          <span lang="tp">sitelen ni li sitelen kepeken sitelen pona e ni</span>
           <span className="entry__sitelenPona__nimi">
             {REGEX_NON_ALPHA.test(glyph?.lasina) || glyph?.lasina}
           </span>
@@ -42,7 +42,7 @@ const EntryComponent = ({glyph, data}) => { // glyph.lasina can be punctuation o
       }
       {data?.sitelen_emosi &&
         <div className="entry__sitelenEmosi">
-          <span lang="tp">sitelen ni la o sitelen kepeken sitelen EMOSI e ni</span>
+          <span lang="tp">sitelen ni li sitelen kepeken sitelen Emosi e ni</span>
           <span className="emoji">
             {data?.sitelen_emosi}
           </span>
@@ -50,7 +50,7 @@ const EntryComponent = ({glyph, data}) => { // glyph.lasina can be punctuation o
       }
       {glyph?.lasina &&
         <div className="entry__sitelenLasina" lang="tp">
-          <span lang="tp">sitelen ni la o sitelen kepeken sitelen LASINA e ni</span>
+          <span lang="tp">sitelen ni li sitelen kepeken sitelen Lasina e ni</span>
           <span className="latin">
             {REGEX_NON_ALPHA.test(glyph?.lasina) || glyph?.lasina}
           </span>
@@ -60,15 +60,15 @@ const EntryComponent = ({glyph, data}) => { // glyph.lasina can be punctuation o
     <div className="entry__body">
       {data && glyph?.lasina &&
         <span className="entry__pronunciation">
-          <span lang="tp">sitelen ni la o toki uta e ni</span>
+          <span lang="tp">sitelen ni li nimi kepeken sitelen suwi e ni</span>
           <span data-sitelen data-sitelen-ratio="0.2" className="entry__pronunciation__glyphs">
             {glyph?.lasina.toUpperCase()}
           </span>
         </span>
       }
-      {data?.def &&
-        <div className="entry__definition">
-          <span lang="tp">sitelen ni la o toki kepeken toki ante e</span>
+      {data?.def
+      ? <div className="entry__definition">
+          <span lang="tp">sitelen ni li toki kepeken toki Inli e</span>
           <div className="latin">
             {
               data?.def?.split?.(' | ALT ')[0]
