39471b147d58de1d4f219a7931cd3fc0fa2be92e
Alexander <alxndr+github@gmail.com>
2024-05-08 11:24:37 -0700

    tidy up use of `useEffect`

--- src/components/analytics.jsx
+++ src/components/analytics.jsx
@@ -1,9 +1,9 @@
 import * as React from 'react'
 
 export default function Analytics() {
-  React.useEffect(() => 
+  React.useEffect(() => {
     window.goatcounter = { path: (window.location.host + "/" + window.location.pathname) }
-  );
+  });
   return <>
     <script
       data-goatcounter="https://alxndr.goatcounter.com/count"
