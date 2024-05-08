459b663439f8eb48a0656f48582db125c1c32d84
Alexander <alxndr+github@gmail.com>
2024-05-08 11:19:51 -0700

    ...don't use a string in `<script>`?

--- src/components/analytics.jsx
+++ src/components/analytics.jsx
@@ -3,11 +3,9 @@ import * as React from 'react'
 export default function Analytics() {
   return <>
     <script>
-      {`
-        window.goatcounter = {
-          path: (location.host + "/" + location.pathname),
-        }
-      `}
+      window.goatcounter = {
+        path: (location.host + "/" + location.pathname),
+      }
     </script>
     <script
       data-goatcounter="https://alxndr.goatcounter.com/count"
